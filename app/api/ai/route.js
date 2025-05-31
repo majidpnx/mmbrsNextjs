12d8bc6d9b66adb07b79ba5bf7afb0ae439de635
majid <majidv9330@gmail.com>
2025-05-31 10:48:10 +0330

    remove package

--- app/api/ai/route.js
+++ app/api/ai/route.js
@@ -1,29 +1,29 @@
-import { NextResponse } from 'next/server';
-import { Together } from 'together-ai';
+// import { NextResponse } from 'next/server';
+// import { Together } from 'together-ai';
  
-const together = new Together({
-  apiKey: '362a84eb9b3e3d7231ab7933e1c85c388080518871975652630eb0bc405b8033',
-});
+// const together = new Together({
+//   apiKey: '362a84eb9b3e3d7231ab7933e1c85c388080518871975652630eb0bc405b8033',
+// });
 
-export async function POST(req) {
-  const body = await req.json();
+// export async function POST(req) {
+//   const body = await req.json();
 
- try {
-    if (body.type === 'image') {
-      const response = await together.images.create({
-        prompt: body.prompt,
-        model: 'black-forest-labs/FLUX.1-dev',
-        steps: 4,
-      });
-      return NextResponse.json({ result: response });
-    } else {
-      const response = await together.chat.completions.create({
-        model: 'meta-llama/Llama-3-8b-chat-hf',
-        messages: body.messages,
-      });
-      return NextResponse.json({ result: response });
-    }
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+//  try {
+//     if (body.type === 'image') {
+//       const response = await together.images.create({
+//         prompt: body.prompt,
+//         model: 'black-forest-labs/FLUX.1-dev',
+//         steps: 4,
+//       });
+//       return NextResponse.json({ result: response });
+//     } else {
+//       const response = await together.chat.completions.create({
+//         model: 'meta-llama/Llama-3-8b-chat-hf',
+//         messages: body.messages,
+//       });
+//       return NextResponse.json({ result: response });
+//     }
+//   } catch (error) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
